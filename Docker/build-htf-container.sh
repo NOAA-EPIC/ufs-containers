31c64ea883a44402e868ae6aab56b0090db1e07b
Yi-Cheng Teng - NOAA <30629225+clouden90@users.noreply.github.com>
2022-10-06 20:39:28 -0400

    Update build-htf-container.sh

--- Docker/build-htf-container.sh
+++ Docker/build-htf-container.sh
@@ -9,10 +9,13 @@ git clone -b ${branch_name} https://github.com/clouden90/ufs-htf.git
 
 #
 cd ufs-htf
-docker build -t clouden90/ubuntu20.04-gnu9.3-hpc-stack-htf-jenkins:test -f ./docker/recipe/Dockerfile.ubuntu20.04-gnu9.3-hpc-stack-htf-jenkins .
+docker build --quiet -t clouden90/ubuntu20.04-gnu9.3-hpc-stack-htf-jenkins:test -f ./docker/recipe/Dockerfile.ubuntu20.04-gnu9.3-hpc-stack-htf-jenkins .
 
 #
+echo "HTF hpc-stack build Start!"
 docker run --user root --rm clouden90/ubuntu20.04-gnu9.3-hpc-stack-htf-jenkins:test /bin/bash -c "bash ./docker/recipe/run_toy.sh"
+echo "HTF hpc-stack build Done!"
 
-#remove 
-docker image rm clouden90/ubuntu20.04-gnu9.3-hpc-stack-htf-jenkins:test
+#remove images
+docker image rmi clouden90/ubuntu20.04-gnu9.3-hpc-stack-htf-jenkins:test &> /dev/null
+docker image rmi noaaepic/ubuntu20.04-gnu9.3-hpc-stack:v1.2b &> /dev/null

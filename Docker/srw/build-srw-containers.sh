17e076f6ec03645407d2aa0b983ee63bcf435894
Mark Potts <Mark.Potts@noaa.gov>
2023-01-05 10:20:17 -0500

    updates for AMS

--- Docker/srw/build-srw-containers.sh
+++ Docker/srw/build-srw-containers.sh
@@ -30,8 +30,8 @@ sed -i "s/TAGNAME/$tag_name/g" Dockerfile.ubuntu20.04-intel-stage3
 docker rm  -f spack-stack
 #Build the final container with the WM and environment variables (path, LD_LIBRARY_PATH, etc.) set for use with singularity
 echo tagname is $tag_name
-docker build --no-cache -f Dockerfile.ubuntu20.04-intel-stage3 -t noaaepic/ubuntu20.04-intel-srwapp:$tag_name .
-#rm Dockerfile.
+#docker build --no-cache -f Dockerfile.ubuntu20.04-intel-stage3 -t noaaepic/ubuntu20.04-intel-srwapp:$tag_name .
+docker build -f Dockerfile.ubuntu20.04-intel-stage3 -t noaaepic/ubuntu20.04-intel-srwapp:$tag_name .
 docker push noaaepic/ubuntu20.04-intel-srwapp:$tag_name 
 rm Dockerfile*stage*
 #Convert the finished docker container into a singularity container for use on HPC systems. 

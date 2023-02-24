8d2729bb2e5892631533f92db46b97082fda6479
Mark A Potts <mark.potts@noaa.gov>
2023-02-24 13:54:38 -0500

    updates for release/public-v1.0.0

--- Docker/landda/run_container_executable.sh
+++ Docker/landda/run_container_executable.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-set -x
 export SINGULARITYENV_FI_PROVIDER=tcp
 export SINGULARITY_SHELL=/bin/bash
 export SINGULARITYENV_APPEND_PATH=/opt/fv3-bundle/build/bin
+BINDDIR="/"`pwd | awk -F"/" '{print $2}'`
 CONTAINERLOC=${EPICCONTAINERS:-${HOME}}
-img=${img:-'${CONTAINERLOC}/ubuntu20.04-intel-spack-landda.img'}
-#img=${CONTAINERLOC}/ubuntu20.04-intel-spack-landda.img
+img=${img:-${CONTAINERLOC}/ubuntu20.04-intel-spack-landda.img}
+CONTAINERBASE="/"`echo $img | xargs realpath | awk -F"/" '{print $2}'`
 cmd=$(basename "$0")
 arg="$@"
-echo running: singularity exec $img $cmd $arg
-singularity exec $img $cmd $arg
+if [ ! -z "$LANDDA_INPUTS" ]; then
+  INPUTBASE="/"`echo $LANDDA_INPUTS | xargs realpath | awk -F"/" '{print $2}'`
+  INPUTBIND="-B $INPUTBASE:$INPUTBASE"
+else
+  INPUTBIND=""
+fi
+echo running: ${SINGULARITYBIN} exec $img $cmd $arg
+${SINGULARITYBIN} exec -B $BINDDIR:$BINDDIR -B $CONTAINERBASE:$CONTAINERBASE $INPUTBIND $img $cmd $arg
 

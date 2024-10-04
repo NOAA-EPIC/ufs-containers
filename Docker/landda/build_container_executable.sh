119bf6e9af4854ea468377213b08707dae97f465
EdwardSnyder-NOAA <Edward.Snyder@noaa.gov>
2024-10-04 17:09:33 +0000

    update script

--- Docker/landda/build_container_executable.sh
+++ Docker/landda/build_container_executable.sh
@@ -3,18 +3,21 @@
 export SINGULARITYENV_FI_PROVIDER=tcp
 export SINGULARITY_SHELL=/bin/bash
 export SINGULARITYENV_JEDI_INSTALL=/opt
+SINGULARITYBIN=`which singularity`
 BINDDIR="/"`pwd | awk -F"/" '{print $2}'`
-CONTAINERLOC=${EPICCONTAINERS:-${HOME}}
-img=${img:-${CONTAINERLOC}/ubuntu20.04-intel-landda-v2.img}
+img=IMAGE
 CONTAINERBASE="/"`echo $img | xargs realpath | awk -F"/" '{print $2}'`
 cmd=$(basename "$0")
 arg="$@"
-if [ ! -z "$LANDDA_INPUTS" ]; then
-  INPUTBASE="/"`echo $LANDDA_INPUTS | xargs realpath | awk -F"/" '{print $2}'`
+if [ ! -z "$LANDDAROOT" ]; then
+  INPUTBASE="/"`echo $LANDDAROOT | xargs realpath | awk -F"/" '{print $2}'`
   INPUTBIND="-B $INPUTBASE:$INPUTBASE"
 else
   INPUTBIND=""
 fi
-echo running: ${SINGULARITYBIN} exec $img $cmd $arg
-${SINGULARITYBIN} exec -e -B $BINDDIR:$BINDDIR -B $CONTAINERBASE:$CONTAINERBASE $INPUTBIND $img $cmd $arg
+# Remove echo for ndate command as it messes with the PTIME variable 
+if [ $cmd != "ndate" ]; then
+  echo running: ${SINGULARITYBIN} exec -B $BINDDIR:$BINDDIR -B $CONTAINERBASE:$CONTAINERBASE $INPUTBIND $img $cmd $arg
+fi
+${SINGULARITYBIN} exec -B $BINDDIR:$BINDDIR -B $CONTAINERBASE:$CONTAINERBASE $INPUTBIND $img $cmd $arg
 

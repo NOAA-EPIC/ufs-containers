8bf8796db646f9873cdfb2a92b8ef0f306da2c75
EdwardSnyder-NOAA <Edward.Snyder@noaa.gov>
2024-10-22 21:35:33 +0000

    add new fflag

--- Docker/landda/build_singularity_intel.lua
+++ Docker/landda/build_singularity_intel.lua
@@ -91,7 +91,7 @@ load("py-pyyaml/6.0")
 
 --load("atlas")
 setenv("CFLAGS","-diag-disable=10448")
-setenv("FFLAGS","-diag-disable=10448")
+setenv("FFLAGS","-I/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/intel/2021.10.0/intel-oneapi-mpi-2021.9.0-6bnjcwc/mpi/2021.9.0//include -I/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/intel/2021.10.0/intel-oneapi-mpi-2021.9.0-6bnjcwc/mpi/2021.9.0/include -L/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/intel/2021.10.0/intel-oneapi-mpi-2021.9.0-6bnjcwc/mpi/2021.9.0/lib/release -L/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/intel/2021.10.0/intel-oneapi-mpi-2021.9.0-6bnjcwc/mpi/2021.9.0/lib -Xlinker --enable-new-dtags -Xlinker -rpath -Xlinker /opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/intel/2021.10.0/intel-oneapi-mpi-2021.9.0-6bnjcwc/mpi/2021.9.0/lib/release -Xlinker -rpath -Xlinker /opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/intel/2021.10.0/intel-oneapi-mpi-2021.9.0-6bnjcwc/mpi/2021.9.0/lib -lmpifort -lmpi -ldl -lrt -lpthread -diag-disable=10448")
 
 setenv("CMAKE_C_COMPILER","mpiicc")
 setenv("CMAKE_CXX_COMPILER","mpicxx")

e446c3f34e800a4978c432f63cb4c8ddc6053ec3
EdwardSnyder-NOAA <edward.snyder@noaa.gov>
2024-11-14 18:37:49 +0000

    fix various bugs

--- Docker/ufs-wm/stage-rt.sh
+++ Docker/ufs-wm/stage-rt.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
-set -x
+#set -x
 
-#DATADIR="home"
-#for i in "$@"
-#do
-#case $i in
-#    -d=*|--datadir=*)
-#    DATADIR="${i#*=}"
-#    ;;
-#    -i=*|--container=*)
-#    IMAGE="${i#*=}"
-#    ;;
-#   --default)
-#   DEFAULT=YES
-#   ;;
-#    *)
-            # unknown option
-#    ;;
-#esac
-#done
 Help()
 {
     # Display Help
@@ -54,6 +36,7 @@ if [ -z "$compiler" ] || [ -z "$image" ] || [ -z "$mpi" ] ; then
 fi
 
 ##singularity exec -H $PWD $IMAGE cp /opt/ufs-weather-model/container-scripts/run_container_executable.sh .
+echo "Copying out ufs-weather-model repo from the container"
 #singularity exec -H $PWD $image cp -r /opt/ufs-weather-model .
 ##mkdir -p bin
 cd ufs-weather-model/bin
@@ -61,27 +44,43 @@ ln -s ../container-scripts/build_container_executable.sh make
 ln -s ../container-scripts/build_container_executable.sh cmake
 ln -s ../container-scripts/run_container_executable.sh python
 cd ../..
+
+# Set the script to run exe in the container
+echo "Set run_test.sh to use exe in the container"
 export line=`/bin/grep -n "cp ${PATHRT}" ufs-weather-model/tests/run_test.sh | /bin/grep fv3.exe | awk -F ":" '{print $1}'`
 sed -i "${line}s/^/#/g"  $PWD/ufs-weather-model/tests/run_test.sh
-sed -i "${line}a ln -s \$\{PATHRT\}\/..\/container-scripts/run_container_executable.sh fv3.exe"  $PWD/ufs-weather-model/tests/run_test.sh
+sed -i "${line}a ln -s \$\{PATHRT\}\/..\/container-scripts/run_container_executable.sh fv3_\$\{COMPILE_ID\}.exe"  $PWD/ufs-weather-model/tests/run_test.sh
 
-# TODO: should we have a specific file, if we use the data locally?
+# Update files with compiler and mpi info
 #sed -i 's/srun/#srun/g' $PWD/ufs-weather-model/tests/fv3_conf/fv3_slurm.IN_singularity
 #sed -i '/#srun/a mpiexec -n @[TASKS] ./fv3.exe' $PWD/ufs-weather-model/tests/fv3_conf/fv3_slurm.IN_singularity
-sed -i "s|COMPILER|$compiler|g" $PWD/ufs-weather-model/tests-dev/test_cases/exp_conf/fv3_slurm.IN_singularity
-sed -i "s|MPI|$mpi|g" $PWD/ufs-weather-model/tests-dev/test_cases/exp_conf/fv3_slurm.IN_singularity
+echo "Updating compiler and mpi in fv3_slurm.IN_singularity"
+sed -i "s|USER_COMPILER|$compiler|g" $PWD/ufs-weather-model/tests-dev/test_cases/exp_conf/fv3_slurm.IN_singularity
+sed -i "s|USER_MPI|$mpi|g" $PWD/ufs-weather-model/tests-dev/test_cases/exp_conf/fv3_slurm.IN_singularity
 
-sed -i "s|SINGULARITY_WORKING_DIR|$PWD|g" $PWD/ufs-weather-model/container-scripts/ufswm.env
-sed -i "s|SINGULARITY_WORKING_DIR|$PWD|g" $PWD/ufs-weather-model/container-scripts/*_executable.sh
+# Create ufs_singularity.intel lua file
+echo "Creating ufs_singularity.intel.lua"
+echo "load(\"$compiler\")" > $PWD/ufs-weather-model/modulefiles/ufs_singularity.intel.lua
+echo "load(\"$mpi\")" >> $PWD/ufs-weather-model/modulefiles/ufs_singularity.intel.lua
 
+# Trick ufs_test.sh file
+echo "Tricking ufs_test.sh file"
+sed -i "174 i export MACHINE_ID=singularity" $PWD/ufs-weather-model/tests-dev/ufs_test.sh
+sed -i "s|s4 )|s4 singularity )|g" $PWD/ufs-weather-model/tests-dev/ufs_test.sh
+
+# Replace with host paths 
+echo "Updating various files with host paths"
 SINGULARITY=`which singularity`
 LOCDIR=`echo $PWD | awk -F "/" '{print $2}'`
 
-#replace the paths in the script
 sed -i "s|IMAGE|$image|g" $PWD/ufs-weather-model/container-scripts/*_executable.sh
 sed -i "s|LOCDIR|$LOCDIR|g" $PWD/ufs-weather-model/container-scripts/*_executable.sh
 sed -i "s|DATADIR|$DATADIR|g" $PWD/ufs-weather-model/container-scripts/*_executable.sh
 #sed -i "s|UFSPATH|$PWD/tests|g" *_executable.sh
 sed -i "s|PATH_TO_SINGULARITY|$SINGULARITY|g" $PWD/ufs-weather-model/container-scripts/*_executable.sh
+sed -i "s|SINGULARITY_WORKING_DIR|$PWD|g" $PWD/ufs-weather-model/container-scripts/ufswm.env
+sed -i "s|SINGULARITY_WORKING_DIR|$PWD|g" $PWD/ufs-weather-model/container-scripts/*_executable.sh
+sed -i "s|SINGULARITY_WORKING_DIR|$PWD|g" $PWD/ufs-weather-model/tests-dev/machine_config/machine_singularity.config
+sed -i "s|SINGULARITY_WORKING_DIR|$PWD|g" $PWD/ufs-weather-model/tests-dev/baseline_setup.yaml
 
 #for FILE in modulefiles/*intel.lua; do echo "prepend_path(\"PATH\", \"$PWD/bin\")" >> $FILE ; done

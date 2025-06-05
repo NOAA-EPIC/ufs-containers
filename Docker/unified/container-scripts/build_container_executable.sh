9f568b623c322cc639c8b7e20fd3ef3070f7690b
Mark A Potts <mark.potts@noaa.gov>
2025-06-05 11:54:17 -0400

    updating

---
+++ Docker/unified/container-scripts/build_container_executable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#set -x
+export img=IMAGE
+export CONTAINERENV_FI_PROVIDER=tcp
+export CONTAINERENV_FI_PROVIDER_PATH=FI_PATH
+export SINGULARITY_SHELL=/bin/bash
+export CONTAINERENV_PATH=EXEC_PATH
+export CONTAINERENV_LD_LIBRARY_PATH=LDLIB_PATH
+export CONTAINERENV_LIBRARY_PATH=LIB_PATH
+export CONTAINERENV_CMAKE_PREFIX_PATH=CMAKE_PREPATH
+export CONTAINERENV_ESMFMKFILE=ESMF_MK
+cmd=BASEFILE
+arg="$@"
+echo running: singularity exec "${img}" $cmd $arg
+singularity exec -e BINDDIRS "${img}" $cmd $arg
+
---
+++ Docker/unified/container-scripts/externalize.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+################################################################################
+# Help                                                                         #
+################################################################################
+Help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: scriptTemplate [-g|h|v|V]"
+   echo "options:"
+   echo "g     Print the GPL license notification."
+   echo "h     Print this Help."
+   echo "v     Verbose mode."
+   echo "V     Print software version and exit."
+   echo
+}
+
+################################################################################
+################################################################################
+# Main program                                                                 #
+################################################################################
+################################################################################
+################################################################################
+# Process the input options. Add options as needed.                            #
+################################################################################
+# Get the options
+while getopts ":he" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      e) # external directory to hold externalized executables
+         exec_dir=$2
+         echo "Will create external executable in $exec_dir"
+   esac
+done
+shift $(($OPTIND ))
+fileList=$@
+
+mkdir -p $exec_dir
+script_dir=$(dirname "$0")
+cp $script_dir/run_container_executable.sh $exec_dir
+#replace CONTAINERENV_ with SINGULARITY/APPTAINER
+if [[ -z $(env | grep APPTAINER) ]]; then 
+   sed -i 's/CONTAINERENV_/SINGULARITYENV_/g' $exec_dir/run_container_executable.sh
+else 
+   sed -i 's/CONTAINERENV_/APPTAINERENV_/g' $exec_dir/run_container_executable.sh
+fi
+#replace the paths in the script
+sed -i "s|IMAGE|$SINGULARITY_CONTAINER|g" $exec_dir/run_container_executable.sh
+nbinds=`echo $SINGULARITY_BIND | awk -F "," '{print NF }'`
+bindstring=" "
+for (( i = 1; i <= $nbinds; i++ )); do binddir=`echo $SINGULARITY_BIND | cut -d "," -f $i` && bindstring="${bindstring} -B ${binddir}" ; done
+echo $bindstring
+sed -i "s|BINDDIRS|$bindstring|g" $exec_dir/run_container_executable.sh
+sed -i "s|LDLIB_PATH|$LD_LIBRARY_PATH|g" $exec_dir/run_container_executable.sh
+sed -i "s|LIB_PATH|$LIBRARY_PATH|g" $exec_dir/run_container_executable.sh
+sed -i "s|FI_PATH|$FI_PROVIDER_PATH|g" $exec_dir/run_container_executable.sh
+
+for file in $fileList
+do
+  fullfile=$(readlink -m $file)
+  basefile=$(basename "$fullfile")
+  cp $exec_dir/run_container_executable.sh $exec_dir/$basefile
+  pathdir=$(dirname $fullfile)
+  echo "fullfile is $fullfile"
+  echo $pathdir
+ 
+  EXEC_PATH="$pathdir:$PATH"
+  sed -i "s|EXEC_PATH|$EXEC_PATH|g" $exec_dir/$basefile
+  sed -i "s|ESMF_MK|$ESMFMKFILE|g" $exec_dir/$basefile
+  sed -i "s|BASEFILE|$basefile|g" $exec_dir/$basefile
+done
+
+chmod +x $exec_dir/*
+rm $exec_dir/run_container_executable.sh

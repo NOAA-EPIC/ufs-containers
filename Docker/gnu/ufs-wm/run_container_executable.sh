fc2437e5f24fc930a5d1e266dd0e2c3ef9bebd62
EdwardSnyder-NOAA <Edward.Snyder@noaa.gov>
2025-12-12 21:03:11 +0000

    init commit

---
+++ Docker/gnu/ufs-wm/run_container_executable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#set -x
+export img=IMAGE
+export SINGULARITYENV_FI_PROVIDER=tcp
+export SINGULARITY_SHELL=/bin/bash
+cmd=$(basename "$0")
+if [[ $cmd == "python3" ]]; then
+    echo "$2" > tmp_arg_file.py
+    arg=tmp_arg_file.py
+    #cat $arg
+else
+    arg="$@"
+fi
+echo running: PATH_TO_SINGULARITY exec --env-file SINGULARITY_WORKING_DIR/ufs-weather-model/container-scripts/ufs-wm.env -B /LOCDIR:/LOCDIR "${img}" $cmd $arg
+PATH_TO_SINGULARITY exec --env-file SINGULARITY_WORKING_DIR/ufs-weather-model/container-scripts/ufs-wm.env -B /LOCDIR:/LOCDIR "${img}" $cmd $arg

21dad95b9525f53e68dbfc92d7386f239f390794
Natalie Perlin <natalie.perlin@noaa.gov>
2026-09-03 01:13:19 -0400

    Updated files to build containers: Rocky9 Intel OneAPI 2024.2.1 + MPI 2021.13, spack-stack 1.9.2 for ufs-wm-env
    branch: feature/unified-1.9.2, location: ./Docker/intel/ufs-wm-env/

--- Docker/intel/ufs-wm-env/intel-sandbox.sh
+++ Docker/intel/ufs-wm-env/intel-sandbox.sh
@@ -1,8 +1,7 @@
-#/bin/bash 
-set -eou pipefail
+#!/bin/bash 
+set -euo pipefail
 
-echo "Build full intel sandbox from Docker intel/hpckit:2024.2.1-0-devel-rockylinux9"
-
+echo "Build a full intel HPC kit as a container sandbox from Docker intel/hpckit:2024.2.1-0-devel-rockylinux9"
 singularity build --sandbox --fix-perms intel-sandbox docker://intel/hpckit:2024.2.1-0-devel-rockylinux9 \
 	2> >(grep -v 'ignoring (usually) harmless EPERM on setxattr ' >&2)
 echo "Done building an intel-sandbox"
